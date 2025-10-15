8a6de166e49636a77d2576cb7e46904f7eee37b3
Stygiansabyss <stygian.warlock.v2@gmail.com>
2025-10-15 10:00:39 -0500

    Heat handling added to options

---
+++ scripts/me5e-options.lua
@@ -0,0 +1,38 @@
+--
+-- Mass Effect 5e Options
+-- Heat handling system options
+--
+
+function onInit()
+    registerME5eOptions();
+end
+
+function registerME5eOptions()
+    -- Heat Handling Option
+    OptionsManager.registerOptionData({
+        sKey = "ME5E_HEAT_HANDLING",
+        sGroupRes = "me5e_option_header_heat",
+        sNameRes = "me5e_option_heat_handling",
+        tCustom = {
+            labelsres = "me5e_option_thermal_clips|me5e_option_venting",
+            values = "thermal_clips|venting",
+            default = "thermal_clips"
+        }
+    });
+end
+
+-- Function to get the current heat handling mode
+function getHeatHandlingMode()
+    return OptionsManager.getOption("ME5E_HEAT_HANDLING");
+end
+
+-- Function to check if thermal clips are enabled
+function isThermalClipsEnabled()
+    return getHeatHandlingMode() == "thermal_clips";
+end
+
+-- Function to check if venting is enabled
+function isVentingEnabled()
+    return getHeatHandlingMode() == "venting";
+end
+
